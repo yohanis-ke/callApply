a0ab43756a1049913e8d06f6d7092853cd80917f
yohanis-ke <yohanes.ke@gmail.com>
2019-02-08 07:59:11 -0500

    error has fixed

--- jason_object.js
+++ jason_object.js
@@ -1,9 +1,9 @@
-var student={
-    "name": "Alex"
-    "ID": 1
-    "Grade": 8
-}
+var student = {
+  name: "Alex",
+  ID: 1,
+  Grade: 8
+};
 
-console.leg(student);
-var myJASON= JASON.stringify(student);
-console.log(myJASON);
+console.log(student);
+var myJSON = JSON.stringify(student);
+console.log(myJSON);
